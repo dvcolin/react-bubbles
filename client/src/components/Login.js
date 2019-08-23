074e5d429535e49108f44565435cb0862a549217
dvcolin <46186956+dvcolin@users.noreply.github.com>
2019-08-23 08:18:21 -0700

    added login form and used formik

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,32 @@
 import React from "react";
+import '../login.scss';
+import { Form, Field, withFormik } from 'formik'
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
   return (
-    <>
+    <div className='login'>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+      <Form className='login-form'>
+      <h2>Login</h2>
+
+      <Field className='login-field' type='text' name='username' placeholder='Enter username' />
+      <Field className='login-field' type='password' name='password' placeholder='Enter password' />
+
+      <button type='submit'>Submit</button>
+      </Form>
+    </div>
   );
 };
 
-export default Login;
+const FormikForm = withFormik({
+  mapPropsToValues({ username, password }) {
+    return {
+      username: username || '',
+      password: password || '',
+    }
+  }
+})(Login)
+
+export default FormikForm;
